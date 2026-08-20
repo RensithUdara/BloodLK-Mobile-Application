39162cdf1e6118c72ed43863bff0eda6a829f4c5
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-20 17:18:49 +0530

    feat: Add AppConstants and AppColors classes for application configuration

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String appName = 'BloodLK';
+  static const String logoAsset = 'assets/blood-lk-logo.png';
+
+  static const List<String> bloodGroups = [
+    'A+',
+    'A-',
+    'B+',
+    'B-',
+    'O+',
+    'O-',
+    'AB+',
+    'AB-',
+  ];
+}
+
+class AppColors {
+  static const Color bloodRed = Color(0xFFE21B23);
+  static const Color deepMaroon = Color(0xFF7A0715);
+  static const Color warmSurface = Color(0xFFFFF7F5);
+  static const Color lightRed = Color(0xFFF7D9D3);
+  static const Color textBrown = Color(0xFF2B171A);
+  static const Color subTextBrown = Color(0xFF7A5A50);
+}
