b459af0c8e2e62650b2e8681598afea4797b5f60
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-21 00:40:54 +0530

    fix: Increase font sizes in _SearchHeader for improved readability

--- lib/views/donor/donor_search_view.dart
+++ lib/views/donor/donor_search_view.dart
@@ -73,7 +73,7 @@ class _SearchHeader extends StatelessWidget {
                     'Find Blood Donors',
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 22,
+                      fontSize: 24,
                       fontWeight: FontWeight.w900,
                     ),
                   ),
@@ -82,7 +82,7 @@ class _SearchHeader extends StatelessWidget {
                     'Search and connect with donors\nin your area.',
                     style: TextStyle(
                       color: Colors.white.withValues(alpha: 0.9),
-                      fontSize: 12,
+                      fontSize: 13,
                       height: 1.35,
                       fontWeight: FontWeight.w500,
                     ),
