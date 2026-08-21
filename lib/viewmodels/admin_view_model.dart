a2c139918949078514bcc2ce761270fabe40679b
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-21 23:54:48 +0530

    feat: Add donationRepository to AdminViewModel and implement watchTotalDonationUnits method

--- lib/viewmodels/admin_view_model.dart
+++ lib/viewmodels/admin_view_model.dart
@@ -3,6 +3,7 @@ import '../data/models/emergency_request.dart';
 import '../data/models/donation_center.dart';
 import '../data/models/donor.dart';
 import '../data/repositories/donation_center_repository.dart';
+import '../data/repositories/donation_repository.dart';
 import '../data/repositories/donor_repository.dart';
 import '../data/repositories/emergency_request_repository.dart';
 import '../data/repositories/notification_repository.dart';
@@ -13,18 +14,21 @@ class AdminViewModel extends ChangeNotifier {
     EmergencyRequestRepository? emergencyRequestRepository,
     NotificationRepository? notificationRepository,
     DonationCenterRepository? donationCenterRepository,
+    DonationRepository? donationRepository,
   })  : _donorRepository = donorRepository ?? DonorRepository(),
         _emergencyRequestRepository =
             emergencyRequestRepository ?? EmergencyRequestRepository(),
         _notificationRepository =
             notificationRepository ?? NotificationRepository(),
         _donationCenterRepository =
-            donationCenterRepository ?? DonationCenterRepository();
+            donationCenterRepository ?? DonationCenterRepository(),
+        _donationRepository = donationRepository ?? DonationRepository();
 
   final DonorRepository _donorRepository;
   final EmergencyRequestRepository _emergencyRequestRepository;
   final NotificationRepository _notificationRepository;
   final DonationCenterRepository? _donationCenterRepository;
+  final DonationRepository? _donationRepository;
 
   Stream<List<Donor>> watchDonors() => _donorRepository.watchAllDonors();
 
@@ -35,6 +39,11 @@ class AdminViewModel extends ChangeNotifier {
 
   Stream<List<EmergencyRequest>> watchEmergencyRequests() {
     return _emergencyRequestRepository.watchOpenRequests();
+  }
+
+  Stream<int> watchTotalDonationUnits() {
+    return (_donationRepository ?? DonationRepository())
+        .watchTotalDonationUnits();
   }
 
   Future<int> sendGroupNotification(String bloodType) {
