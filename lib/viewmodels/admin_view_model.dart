2258aaf8190e7a27441a38acbfa380e95612ab9a
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-21 23:37:05 +0530

    feat: Enhance AdminViewModel to include DonationCenterRepository and add methods for watching and saving donation centers

--- lib/viewmodels/admin_view_model.dart
+++ lib/viewmodels/admin_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../data/models/emergency_request.dart';
+import '../data/models/donation_center.dart';
 import '../data/models/donor.dart';
+import '../data/repositories/donation_center_repository.dart';
 import '../data/repositories/donor_repository.dart';
 import '../data/repositories/emergency_request_repository.dart';
 import '../data/repositories/notification_repository.dart';
@@ -10,17 +12,26 @@ class AdminViewModel extends ChangeNotifier {
     DonorRepository? donorRepository,
     EmergencyRequestRepository? emergencyRequestRepository,
     NotificationRepository? notificationRepository,
+    DonationCenterRepository? donationCenterRepository,
   })  : _donorRepository = donorRepository ?? DonorRepository(),
         _emergencyRequestRepository =
             emergencyRequestRepository ?? EmergencyRequestRepository(),
         _notificationRepository =
-            notificationRepository ?? NotificationRepository();
+            notificationRepository ?? NotificationRepository(),
+        _donationCenterRepository =
+            donationCenterRepository ?? DonationCenterRepository();
 
   final DonorRepository _donorRepository;
   final EmergencyRequestRepository _emergencyRequestRepository;
   final NotificationRepository _notificationRepository;
+  final DonationCenterRepository? _donationCenterRepository;
 
   Stream<List<Donor>> watchDonors() => _donorRepository.watchAllDonors();
+
+  Stream<List<DonationCenter>> watchDonationCenters() {
+    return (_donationCenterRepository ?? DonationCenterRepository())
+        .watchCenters();
+  }
 
   Stream<List<EmergencyRequest>> watchEmergencyRequests() {
     return _emergencyRequestRepository.watchOpenRequests();
@@ -32,5 +43,10 @@ class AdminViewModel extends ChangeNotifier {
 
   Future<void> postEmergencyRequest(EmergencyRequest request) {
     return _emergencyRequestRepository.saveRequest(request);
+  }
+
+  Future<void> saveDonationCenter(DonationCenter center) {
+    return (_donationCenterRepository ?? DonationCenterRepository())
+        .saveCenter(center);
   }
 }
